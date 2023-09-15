e315635e4c765977cbcd573c067a1f1974d22a5b
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-15 11:46:10 +1000

    create basic functions

--- main.js
+++ main.js
@@ -32,13 +32,13 @@ const secondNumber = 0;
 
 function operate(operator, num1, num2) {
   switch (operator) {
-    case "+";
-    return add(num1, num2)
-    case "-";
-    return subtract(num1, num2)
-    case "*";
-    return multiply(num1, num2)
-    case "/";
-    return divide(num1, num2)
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
   }
 }
