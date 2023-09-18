e6a101190306fed3be4bb7cda487b3fbf06cdf7c
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-18 12:42:38 +1000

    grab all elements

---
+++ script.js
@@ -0,0 +1,35 @@
+const previousOperand = document.querySelector("[data-previous-operand]");
+const currentOperand = document.querySelector("[data-current-operand]");
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]")
+const equalsButton - document.querySelector("[data-equals]")
+const deleteButton - document.querySelector("[data-delete]")
+const clearButton - document.querySelector("[data-clear-all]")
+
+
+
+
+
+
+
+// let calculation = [];
+// let userInput;
+
+// function calculate(button) {
+//   const value = button.textContent;
+//   if (value === "CLEAR") {
+//     calculation = [];
+//     currentOperand.textContent = "";
+//   } else if (value === "=") {
+//     currentOperand.textContent = eval(userInput);
+//   } else {
+//     calculation.push(value);
+//     userInput = calculation.join("");
+//     currentOperand.textContent = userInput;
+//   }
+// }
+
+// buttons.forEach((button) => button.addEventListener("click", () => calculate(button)));
+
+// console.log(buttons);
