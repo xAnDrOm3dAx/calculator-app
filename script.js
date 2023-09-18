c4ff93649f9f3bfb7d024563be2d76e059cec7d9
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-18 13:11:17 +1000

    add class and functions

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
+class Calculator {
+  constructor(previousOperand, currentOperand) {
+    this.previousOperand = previousOperand;
+    this.currentOperand = currentOperand;
+    this.clear();
+  }
+
+  clear() {}
+
+  delete() {}
+
+  appendNumber(number) {}
+
+  selectOperator(operation) {}
+
+  compute() {}
+
+  updateDisplay() {}
+}
+
 const previousOperand = document.querySelector("[data-previous-operand]");
 const currentOperand = document.querySelector("[data-current-operand]");
-
 const numberButtons = document.querySelectorAll("[data-number]");
-const operatorButtons = document.querySelectorAll("[data-operator]")
-const equalsButton - document.querySelector("[data-equals]")
-const deleteButton - document.querySelector("[data-delete]")
-const clearButton - document.querySelector("[data-clear-all]")
-
-
-
-
-
-
-
-// let calculation = [];
-// let userInput;
-
-// function calculate(button) {
-//   const value = button.textContent;
-//   if (value === "CLEAR") {
-//     calculation = [];
-//     currentOperand.textContent = "";
-//   } else if (value === "=") {
-//     currentOperand.textContent = eval(userInput);
-//   } else {
-//     calculation.push(value);
-//     userInput = calculation.join("");
-//     currentOperand.textContent = userInput;
-//   }
-// }
-
-// buttons.forEach((button) => button.addEventListener("click", () => calculate(button)));
-
-// console.log(buttons);
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const clearButton = document.querySelector("[data-clear-all]");
