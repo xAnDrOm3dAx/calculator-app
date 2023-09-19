04b61081e41fede2715dd9d387d43f6cdc3e6c88
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-19 12:41:45 +1000

    grab elements from HTML

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const previousOperandElement = document.querySelector("[data-previous-operand]");
+const currentOperandElement = document.querySelector("[data-current-operand]");
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const clearButton = document.querySelector("[data-clear-all]");
+
 // Functions
 
 function add(num1, num2) {
@@ -5,28 +14,43 @@ function add(num1, num2) {
 }
 // const add = (num1, num2) => num1 + num2;
 
+// const add = function (a, b) {
+//   return a + b;
+// }
+
 function subtract(num1, num2) {
   return num1 - num2;
 }
 // const subtract = (num1, num2) => num1 - num2;
+
+// const subtract = function (a, b) {
+//   return a - b;
+// }
 
 function multiply(num1, num2) {
   return num1 * num2;
 }
 // const multiply = (num1, num2) => num1 * num2;
 
+// const multiply = function (a, b) {
+//   return a * b;
+// }
+
 function divide(num1, num2) {
   return num1 / num2;
 }
 
-console.log(divide(10, 2));
 // const divide = (num1, num2) => num1 / num2;
+
+// const divide = function (a, b) {
+//   return a / b;
+// }
 
 // Variables
 
-let firstNumber = 0;
-let operator = "+";
-const secondNumber = 0;
+let currentValue = 0;
+const previousValue = 0;
+let operator = "";
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
