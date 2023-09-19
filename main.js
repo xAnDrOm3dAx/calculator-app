b44201e4e88a71fc0044731e932cb016224147ad
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-19 17:04:30 +1000

    get buttons to reflect value on screen

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-const previousOperandElement = document.querySelector("[data-previous-operand]");
-const currentOperandElement = document.querySelector("[data-current-operand]");
+document.addEventListener("DOMContentLoaded", function () {
+  // Grab all HTML elements
+  const previousOperandElement = document.querySelector("[data-previous-operand]");
+  const currentOperandElement = document.querySelector("[data-current-operand]");
 
-const numberButtons = document.querySelectorAll("[data-number]");
-const operatorButtons = document.querySelectorAll("[data-operator]");
-const equalsButton = document.querySelector("[data-equals]");
-const deleteButton = document.querySelector("[data-delete]");
-const clearButton = document.querySelector("[data-clear-all]");
+  const numberButtons = document.querySelectorAll("[data-number]");
+  const operatorButtons = document.querySelectorAll("[data-operator]");
 
-// Functions
+  const decimalButton = document.querySelector("[data-decimal]");
+  const equalsButton = document.querySelector("[data-equals]");
+  const deleteButton = document.querySelector("[data-delete]");
+  const clearButton = document.querySelector("[data-clear-all]");
+
+  numberButtons.forEach((number) =>
+    number.addEventListener("click", function (e) {
+      handleDataNumber(e.target.textContent);
+      currentOperandElement.textContent = currentValue;
+    })
+  );
+});
+
+// Global Variables
+
+let currentValue = "";
+const previousValue = "";
+let operator = "";
+let result = 0;
+
+// Calc Functions
 
 function add(num1, num2) {
   return num1 + num2;
@@ -46,23 +65,27 @@ function divide(num1, num2) {
 //   return a / b;
 // }
 
-// Variables
+// Create a new function calculate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
-let currentValue = 0;
-const previousValue = 0;
-let operator = "";
+function handleDataNumber(number) {
+  currentValue += number;
+}
 
-// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function handleDataOperator() {}
 
-function operate(operator, num1, num2) {
-  switch (operator) {
-    case "+":
-      return add(num1, num2);
-    case "-":
-      return subtract(num1, num2);
-    case "*":
-      return multiply(num1, num2);
-    case "/":
-      return divide(num1, num2);
+function calculate() {
+  if (operator === "") {
+    return;
+  } else {
+    switch (operator) {
+      case "+":
+        result = add(num1, num2);
+      case "-":
+        result = subtract(num1, num2);
+      case "*":
+        result = multiply(num1, num2);
+      case "/":
+        result = divide(num1, num2);
+    }
   }
 }
