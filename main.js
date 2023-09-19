b250eea45fd483a72953275148037ee558754fe4
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-09-19 20:43:58 +1000

    organise code, add clear functionality

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Grab all HTML elements
-  const previousOperandElement = document.querySelector("[data-previous-operand]");
-  const currentOperandElement = document.querySelector("[data-current-operand]");
+  const topDisplay = document.querySelector("[data-top-display]");
+  const bottomDisplay = document.querySelector("[data-bottom-display]");
 
   const numberButtons = document.querySelectorAll("[data-number]");
   const operatorButtons = document.querySelectorAll("[data-operator]");
@@ -11,20 +11,61 @@ document.addEventListener("DOMContentLoaded", function () {
   const deleteButton = document.querySelector("[data-delete]");
   const clearButton = document.querySelector("[data-clear-all]");
 
+  // Add event listener for each number button
   numberButtons.forEach((number) =>
     number.addEventListener("click", function (e) {
       handleDataNumber(e.target.textContent);
-      currentOperandElement.textContent = currentValue;
+      bottomDisplay.textContent = currentValue;
     })
   );
+
+  // Add event listener for clear button. Once clicked, return all values to "".
+  clearButton.addEventListener("click", function () {
+    currentValue = ""; // Clear the current value
+    previousValue = "";
+    operator = "";
+    topDisplay.textContent = ""; // Update the display
+    bottomDisplay.textContent = "";
+  });
 });
 
 // Global Variables
 
 let currentValue = "";
-const previousValue = "";
+let previousValue = "";
 let operator = "";
-let result = 0;
+let result = "";
+
+// Create a new function calculate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function handleDataNumber(number) {
+  if (currentValue.length < 12) {
+    currentValue += number;
+  }
+}
+
+function handleDataOperator() {}
+
+function deleteLastNumber() {
+  //slice(0, -1)
+}
+
+function calculate() {
+  if (operator === "") {
+    return;
+  } else {
+    switch (operator) {
+      case "+":
+        result = add(num1, num2);
+      case "-":
+        result = subtract(num1, num2);
+      case "*":
+        result = multiply(num1, num2);
+      case "/":
+        result = divide(num1, num2);
+    }
+  }
+}
 
 // Calc Functions
 
@@ -59,33 +100,8 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-// const divide = (num1, num2) => num1 / num2;
-
 // const divide = function (a, b) {
 //   return a / b;
 // }
 
-// Create a new function calculate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-
-function handleDataNumber(number) {
-  currentValue += number;
-}
-
-function handleDataOperator() {}
-
-function calculate() {
-  if (operator === "") {
-    return;
-  } else {
-    switch (operator) {
-      case "+":
-        result = add(num1, num2);
-      case "-":
-        result = subtract(num1, num2);
-      case "*":
-        result = multiply(num1, num2);
-      case "/":
-        result = divide(num1, num2);
-    }
-  }
-}
+// const divide = (num1, num2) => num1 / num2;
